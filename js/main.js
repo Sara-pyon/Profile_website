585fd4ec0faf8e85559fb192ed98cb2f91725206
Sara-pyon <149462367+Sara-pyon@users.noreply.github.com>
2023-12-13 20:31:58 -0700

    made a aboutMe section

--- js/main.js
+++ js/main.js
@@ -5,3 +5,12 @@ const menu = document.getElementById('menu'),
 
 toggleOpen.addEventListener('click', () => menu.classList.add('show-menu'));
 toggleClose.addEventListener('click', () => menu.classList.remove('show-menu'));
+
+// ===================== Show Education ========================================
+const historyBtn = document.getElementById('history'),
+      educationBtn = document.getElementById('education'),
+      detailBody = document.getElementById('aboutMe-detail');
+
+      historyBtn.addEventListener('click', () => detailBody.classList.remove('show-education'));
+      educationBtn.addEventListener('click', () => detailBody.classList.add('show-education'));
+
