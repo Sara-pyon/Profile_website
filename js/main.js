280a40e651c6b467543ece7530ac249a34caed54
Sara-pyon <149462367+Sara-pyon@users.noreply.github.com>
2023-12-14 14:27:40 -0700

    build a portfolio section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+import { websites } from "./websites.js";
 // ===================== Show Menu ========================================
 const menu = document.getElementById('menu'),
       toggleOpen = document.getElementById('toggle-open'),
@@ -14,3 +15,48 @@ const historyBtn = document.getElementById('history'),
       historyBtn.addEventListener('click', () => detailBody.classList.remove('show-education'));
       educationBtn.addEventListener('click', () => detailBody.classList.add('show-education'));
 
+// ===================== Portfolio Cards ========================================
+const portfolioCards = document.getElementById('portfolio-cards');
+let portfolioHTML =`<div class="swiper-wrapper">`;
+
+websites.forEach(item => {
+    portfolioHTML += `
+    
+    <div class="portfolio__card swiper-slide">
+        <div class="portfolio__img">
+            <img src="${item.image}" alt="website image">
+            <div class="portfolio__git-div">
+                <a target="_blank" href="${item.github}">
+                <i class="ri-github-fill portfolio__git"></i>
+                </a>
+            </div>
+        </div>
+        <h3 class="portfolio__title">${item.title}</h3>
+        <p class="portfolio__text">${item.text}</p>
+        <button class="portfolio__btn">
+            <a target="_blank" href="${item.url}">
+                visit</a></button>
+    </div>
+    `
+})
+portfolioHTML += `</div> <div class="swiper-button-prev"></div>
+<div class="swiper-button-next"></div>`
+
+portfolioCards.innerHTML = portfolioHTML;
+
+
+
+
+
+// ===================== Portfolio Swiper ========================================
+const swiperHome = new Swiper('.portfolio__swiper', {
+    spaceBetween: 10,
+    grapCursor: true,
+    slidesPerView: 'auto',
+    centeredSlides: 'auto',
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+  });
---
+++ js/websites.js
@@ -0,0 +1,31 @@
+export const websites = [{
+    title: "E-book website",
+    image: "image/Portfolio-1.png",
+    text: 'Designed by Bedimcode',
+    url: "https://e-bookwebsite.netlify.app/",
+    github: "https://github.com/Sara-pyon/e-book_website"
+},{
+    title: "Christmas website",
+    image: "image/Portfolio-2.png",
+    text: 'Designed by Bedimcode',
+    url: 'https://main--christmas-websitebysara.netlify.app/',
+    github: 'https://github.com/Sara-pyon/Christmas_website'
+},{
+    title: "Youtube Sidebar",
+    image: "image/Portfolio-3.png",
+    text: 'Designed by Bedimcode',
+    url: 'https://youtube-sidebar.netlify.app/',
+    github: '',
+},{
+    title: "Travel website",
+    image: "image/Portfolio-4.png",
+    text: 'Designed by Bedimcode',
+    url: 'https://6578bae32a5c070008dddebd--travel-websitebysara.netlify.app/',
+    github: 'https://github.com/Sara-pyon/Travel_website'
+},{
+    title: "Moshify website",
+    image: "image/Portfolio-5.png",
+    text: 'Designed by Mosh Hamadani',
+    url: 'https://654af43db57e610008cca27b--moshfiedbysara.netlify.app/',
+    github: 'https://github.com/Sara-pyon/Sarafield'
+}]
